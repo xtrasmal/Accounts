4788cbac0fe3f021801000460ca675350b333153
netbulae <xander@netbulae.eu>
2014-06-08 21:18:52 +0200

    added stuff

---
+++ controllers/LoginFormController.php
@@ -0,0 +1,10 @@
+<?php namespace App\Modules\Accounts\Controllers;
+
+use Ill\System\Base\BaseController;
+
+class LoginFormController extends BaseController
+{
+
+
+
+}
---
+++ models/LoginForm.php
@@ -0,0 +1,11 @@
+<?php namespace App\Modules\Accounts\Models;
+
+use Ill\System\Base\BaseForm;
+
+class LoginForm extends BaseForm
+{
+
+
+
+
+}
